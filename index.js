773eda504bdecfe6219792f5dfda9b0fe1d188be
igorcooli <igorcooli@gmail.com>
2022-07-03 14:32:38 -0300

    quicky mofication

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
 const express = require('express')
 const app = express()
 const TweetController = require('./controller/tweetController')
-const KeepAliveService = require('./service/KeepAliveService')
 
 let controller = new TweetController()
-let keepAliveService = new KeepAliveService()
-
-// keepAliveService.keepAlive()
 
 app.get('/run', function (req, res) {
     res.send('Starting stream!')
